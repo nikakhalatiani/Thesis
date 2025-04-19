c3cd3433d9e335ad6eb889875c4c5f104317082e
Nikolai Khalatiani <khalatiani.nikolai@kiu.edu.ge>
2025-04-19 13:17:59 +0400

    added docs and types

--- InputParser.py
+++ InputParser.py
@@ -1,16 +1,43 @@
-from fandango import FandangoParseError
+from typing import Any
+
+from fandango import FandangoParseError, Fandango
+from fandango.language.tree import DerivationTree
+
+from collections.abc import Callable
+
 
 class InputParser:
-    def __init__(self, extraction_strategy):
+    """
+    A parser that extracts inputs from a derivation tree using a specified extraction strategy.
+    """
+
+    def __init__(self, extraction_strategy: Callable):
         """
-        extraction_strategy: A function that takes a parse tree and returns inputs
+        Initialize the InputParser with an extraction strategy.
+
+        Args:
+            extraction_strategy: A function that takes a parse tree and returns the extracted inputs.
         """
         self.extraction_strategy = extraction_strategy
 
-    def parse(self, fan, fuzzed_string):
+    def parse(self, fan: Fandango, tree: DerivationTree) -> Any | None:
+        """
+        Parse a derivation tree and extract inputs using the extraction strategy.
+
+        Args:
+            fan: The Fandango object used for parsing.
+            tree: The derivation tree to parse.
+
+        Returns:
+            The extracted inputs if parsing is successful, otherwise None.
+
+        Raises:
+            FandangoParseError: If parsing fails, an error message is printed.
+        """
         try:
-            for tree in fan.parse(fuzzed_string):
+            # TODO ask if parsing DerivationTree is better than parsing str
+            for tree in fan.parse(tree):
                 return self.extraction_strategy(tree)
         except FandangoParseError as e:
-            print(f"❌ Parsing failed at position {e.position} in '{fuzzed_string}'")
+            print(f"❌ Parsing failed at position {e.position} in '{tree}'")
         return None
